92cdeee5019222d0657ef3cd1c13da25e4bcc14e
Rachmad Aziz Fazarikha <113221795+RAFazarikha@users.noreply.github.com>
2025-01-14 10:33:33 +0700

    Revert "anasikhzf"

--- component/top-bar.php
+++ component/top-bar.php
@@ -1,6 +1,15 @@
 <div class="top-bar">
     <div class="container-fluid">
         <div class="row align-items-center">
+            <div class="col-md-12">
+                <div class="logo">
+                    <a href="index.html">
+                        <h1>PT. RIQKI INTIPRATAMA SINERGI</h1>
+                        <!-- <img src="img/logo.jpg" alt="Logo"> -->
+                    </a>
+                </div>
+            </div>
+            <!-- <div class="col-lg-8 col-md-7 d-none d-lg-block">
                 <div class="row">
                     <div class="col-4">
                         <div class="top-bar-item">
@@ -13,25 +22,25 @@
                             </div>
                         </div>
                     </div>
-                    <div class="col-5">
+                    <div class="col-4">
                         <div class="top-bar-item">
                             <div class="top-bar-icon">
                                 <i class="flaticon-call"></i>
                             </div>
                             <div class="top-bar-text">
                                 <h3>Call Us</h3>
-                                <p>+62 8151 5466 619</p>
+                                <p>+012 345 6789</p>
                             </div>
                         </div>
                     </div>
-                    <div class="col-3">
+                    <div class="col-4">
                         <div class="top-bar-item">
                             <div class="top-bar-icon">
                                 <i class="flaticon-send-mail"></i>
                             </div>
                             <div class="top-bar-text">
                                 <h3>Email Us</h3>
-                                <p>rizkiintipratamasinergi@gmail.com</p>
+                                <p>info@example.com</p>
                             </div>
                         </div>
                     </div>
